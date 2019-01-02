231feca48efe47bae10082fb633af809b2ff625e
Fawzi Benamor <f.benamor@root.sa>
2019-01-02 11:16:25 +0100

    first commit

---
+++ src/main/java/com/fawzi/restgestionstock/RestGestionStockApplication.java
@@ -0,0 +1,14 @@
+package com.fawzi.restgestionstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestGestionStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestGestionStockApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/fawzi/restgestionstock/RestGestionStockApplicationTests.java
@@ -0,0 +1,17 @@
+package com.fawzi.restgestionstock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestGestionStockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
